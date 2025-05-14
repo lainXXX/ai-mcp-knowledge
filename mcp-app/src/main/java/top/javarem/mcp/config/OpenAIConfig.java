a2bdacf5137ee3c5e8c6887081a7a3e521ed23f3
lain <2786338588@qq,com>
2025-05-14 11:15:27 +0800

    升级工程

--- mcp-app/src/main/java/top/javarem/mcp/config/OpenAIConfig.java
+++ mcp-app/src/main/java/top/javarem/mcp/config/OpenAIConfig.java
@@ -1,7 +1,6 @@
 package top.javarem.mcp.config;
 
-import org.springframework.ai.openai.OpenAiEmbeddingModel;
-import org.springframework.ai.openai.api.OpenAiApi;
+
 import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.ai.vectorstore.pgvector.PgVectorStore;
@@ -19,40 +18,40 @@ public class OpenAIConfig {
     }
 
 
-    @Bean
-    public OpenAiApi openAiApi(@Value("${spring.ai.openai.base-url}") String baseUrl, @Value("${spring.ai.openai.api-key}") String apikey) {
-        return OpenAiApi.builder()
-                .baseUrl(baseUrl)
-                .apiKey(apikey)
-                .build();
-    }
-
-    @Bean("openAiSimpleVectorStore")
-    public SimpleVectorStore vectorStore(OpenAiApi openAiApi) {
-        OpenAiEmbeddingModel embeddingModel = new OpenAiEmbeddingModel(openAiApi);
-        return SimpleVectorStore.builder(embeddingModel).build();
-    }
-
-    /**
-     * -- 删除旧的表（如果存在）
-     * DROP TABLE IF EXISTS public.vector_store_openai;
-     *
-     * -- 创建新的表，使用UUID作为主键
-     * CREATE TABLE public.vector_store_openai (
-     *     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-     *     content TEXT NOT NULL,
-     *     metadata JSONB,
-     *     embedding VECTOR(1536)
-     * );
-     *
-     * SELECT * FROM vector_store_openai
-     */
-    @Bean("openAiPgVectorStore")
-    public PgVectorStore pgVectorStore(OpenAiApi openAiApi, JdbcTemplate jdbcTemplate) {
-        OpenAiEmbeddingModel embeddingModel = new OpenAiEmbeddingModel(openAiApi);
-        return PgVectorStore.builder(jdbcTemplate, embeddingModel)
-                .vectorTableName("vector_store_openai")
-                .build();
-    }
+//    @Bean
+//    public OpenAiApi openAiApi(@Value("${spring.ai.openai.base-url}") String baseUrl, @Value("${spring.ai.openai.api-key}") String apikey) {
+//        return OpenAiApi.builder()
+//                .baseUrl(baseUrl)
+//                .apiKey(apikey)
+//                .build();
+//    }
+//
+//    @Bean("openAiSimpleVectorStore")
+//    public SimpleVectorStore vectorStore(OpenAiApi openAiApi) {
+//        OpenAiEmbeddingModel embeddingModel = new OpenAiEmbeddingModel(openAiApi);
+//        return SimpleVectorStore.builder(embeddingModel).build();
+//    }
+//
+//    /**
+//     * -- 删除旧的表（如果存在）
+//     * DROP TABLE IF EXISTS public.vector_store_openai;
+//     *
+//     * -- 创建新的表，使用UUID作为主键
+//     * CREATE TABLE public.vector_store_openai (
+//     *     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+//     *     content TEXT NOT NULL,
+//     *     metadata JSONB,
+//     *     embedding VECTOR(1536)
+//     * );
+//     *
+//     * SELECT * FROM vector_store_openai
+//     */
+//    @Bean("openAiPgVectorStore")
+//    public PgVectorStore pgVectorStore(OpenAiApi openAiApi, JdbcTemplate jdbcTemplate) {
+//        OpenAiEmbeddingModel embeddingModel = new OpenAiEmbeddingModel(openAiApi);
+//        return PgVectorStore.builder(jdbcTemplate, embeddingModel)
+//                .vectorTableName("vector_store_openai")
+//                .build();
+//    }
 
 }
